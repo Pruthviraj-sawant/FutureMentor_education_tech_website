7676e80c7e6efaf3383da6d19daaf2d0aa1c1365
pruthviraj-sawant <sawantpruthviraj1@gmail.com>
2025-01-20 20:01:33 +0530

    semi-final

--- FUTUREMENTOR_WEB/server/index.js
+++ FUTUREMENTOR_WEB/server/index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		// origin:"http://localhost:3000",
+		origin:"future-mentor-education-tech-website-frontend.vercel.app",
 		credentials:true,
 	})
 )
